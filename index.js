df78e20990da60438d94b077831d1efc1093022f
hyeseong-lee <hyeseong43@gmail.com>
2021-05-21 12:39:00 +0900

    Express Core: Middlewares

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-// const express = require('express');
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 const app = express();
 
@@ -8,12 +11,33 @@ const PORT = 4000;
 const handleListening = () => 
     console.log(`Listening on: http://localhost:${PORT}`);
 
-
-const handleHome = (req, res) => res.send('Hello from Home')
+const handleHome = (req, res) => res.send('미들웨어 후에 잘 나오네요.')
 const handleProfile = (req, res) => res.send('프로파일입니다.')
+const betweenHome = (req, res, next) => {
+    console.log("나는 중간에 있어요.")
+    next();
+}
+const afterHome = (req, res, next) => {
+    console.log("나는 두번째 미들웨어");
+    next();
+}
 
-app.get("/",handleHome);
-app.get("/profileeeee",handleProfile);
+// const middleware = (req, res, next) => {
+//     res.send("not happening");
+//     next();
+
+app.use([
+    cookieParser(),
+    bodyParser.json(),                        // json데이터 처리
+    bodyParser.urlencoded({extended: true}), // http form 데이터 처리
+    morgan("dev"),
+    helmet(),
+    betweenHome, 
+    afterHome
+]); 
+
+app.get("/", handleHome );
+app.get("/profile",handleProfile);
 
 app.listen(PORT, handleListening);
 
