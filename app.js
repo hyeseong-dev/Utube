b420efb14ab04da6f756d96da6ae03bd70f1dc0c
hyeseong-lee <hyeseong43@gmail.com>
2021-05-22 07:44:37 +0900

    Local Variables in Pug 1. 템플릿 엔진 퍼그를 이용하여 프런트 단의 효율성 추구 2. views/ 폴더 아래 하위 디렉토리와 pug파일들을 각 구성(layouts/, partials/, ... pug files) 3. app.js 파일은 divide&conquer 방식을 이용   - 프로젝트 디렉토리에서 middlewares 모듈을 생성 localsMiddleware로 함수구성을 하며 export로 외부 접근을 허용합니다.   - 그리고 res(response)객체의 locals속성에서 siteName과 routes속성에 값을 할당하여 서비스 로직을 구성합니다. 4. 3번의 서비스 로직에서 구성한 속성값은 main.pub, footer.pub, header.pub 파일에서 pug에서 템플릿 변수로 처리하도록 #{?} 형식으로 처리됩니다.

--- app.js
+++ app.js
@@ -3,24 +3,24 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
 
 const app = express();
 
-app.set('view engine', "pug");
-app.use([
-    cookieParser(),
-    bodyParser.json(),                        // json데이터 처리
-    bodyParser.urlencoded({extended: true}), // http form 데이터 처리
-    helmet(),
-    morgan("dev"),
-]); 
+app.use(helmet());
+app.set("view engine", "pug");
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(localsMiddleware);
 
-app.use(routes.videos, videoRouter);
-app.use(routes.users, userRouter);
 app.use(routes.home, globalRouter); 
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
