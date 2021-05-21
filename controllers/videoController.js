ebbe9db00b1d920ff485fa4a5122eec0a0265a17
hyeseong-lee <hyeseong43@gmail.com>
2021-05-22 08:27:23 +0900

    Search Controller I. 검색 기능 1. header.pub에서 .header__column을 추가 삽입합니다. 그래고 행을 바꾸고 form태그를 작성하는데요 이때 action속성에 routes.search를 사용해주는게 포인트입니다. 추가적으로 search form을 완성하기 위한 input태그를 마무리합니다. 2. 검색창에 엔터를 했을때 이동해야할 search.pug에서는 어떻게 받을지 처리해야하는데요. - .search__header를 작성하고 아래 h3 Searching for: 로 문자 처리를 하고 controller에서 받아올 #{searchingBy}라고 네이밍처리를 합니다. 3. 컨트롤 로직의 경우 search함수에서 내부 비즈니스 로직을 정의할 텐데요. ES6방식으로 객체 정의를 합니다. req객체를 우항에 받고 좌항에 nested방식으로 속성인 query를 키로 다시 value 자리에 객체 형식으로 term: searchingBy라고 하는데요 마지막 searchingBy가 render함수의 두번쨰 매개변수의 객체의 2번째 객체가됩니다.

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,8 +1,12 @@
 export const home = (req, res) => 
 res.render("home", {pageTitle: "Home"}); // 첫번째는 템플릿 두번째는 템플릿에 전달될 객체 정보
 
-export const search = (req, res) => 
-res.render("search", {pageTitle: "Search"});
+export const search = (req, res) => {
+    const {
+      query: { term: searchingBy }
+    } = req;
+    res.render("search", { pageTitle: "Search", searchingBy });
+  };
 
 export const upload = (req, res) => 
 res.render("upload", {pageTitle: "Upload"});
